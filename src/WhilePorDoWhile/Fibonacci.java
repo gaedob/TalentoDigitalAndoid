5a44868b5c0d3b3e41c6020caf0b910df8a7d62c
Giovanni Edgardo Aedo Benavides <gaedob@gmail.com>
2024-07-14 00:16:21 -0400

    Desafío guiado - While por do while

--- src/WhilePorDoWhile/Fibonacci.java
+++ src/WhilePorDoWhile/Fibonacci.java
@@ -1,33 +1,23 @@
 package WhilePorDoWhile;
-
 import java.util.Scanner;
 
 public class Fibonacci {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         // Pedir al usuario que ingrese el valor de n
         System.out.print("Ingrese elementos a mostrar Fibonacci: ");
         int fibo = scanner.nextInt();
-
         // Mostrar los primeros n números pares
         mostrarFibos(fibo);
 
     }
-
     private static void mostrarFibos(int fibo) {
         // TODO Auto-generated method stub
         int suma = 0;
-
         for (int num = 0; num <= fibo; num++) {
-
             suma = fibo(num);
             System.out.println(suma);
-
         }
-
     }
 
     private static int fibo(int valor) {
@@ -49,7 +39,6 @@ public class Fibonacci {
             ciclofibo++;
         }
     }
-       
         return sumafibo;
     }
 
